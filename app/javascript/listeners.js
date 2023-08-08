c7964bd84e192f5cfd28a1fe78cedb3a338e64d5
Darcy <darcy.lauder@shaw.ca>
2023-08-08 13:32:21 -0600

    add click listener for page to get rid of sub-link

--- app/javascript/listeners.js
+++ app/javascript/listeners.js
@@ -1,11 +1,27 @@
 const navElements = document.getElementsByClassName("container__nav__link");
-const pageElements = document.getElementsByClassName("");
+const pageElements = document.getElementsByClassName("container__page");
+const mainContainer = document.getElementsByClassName("container")[0];
+const navContainer = document.getElementsByClassName("contaner_nav");
+
+mainContainer.addEventListener("click", function() {
+  for (page of pageElements) {
+    page.style.display = "none";
+  }
+});
+
+
+
 for (element of navElements) {
   element.addEventListener("mouseenter", function() {
-    console.log(navElements);
+    for (page of pageElements) {
+      page.style.display = "none";
+    }
     page = this.id.split('__')[0];
-    pageContainerId = `${page}__container`;
-    pageContainer = document.getElementById(pageContainerId);
+    pageContainer = document.getElementById(`${page}__container`);
     pageContainer.style.display = 'flex';
-  });  
+  });
+
+  element.addEventListener("click", function() {
+    navContainer.style.left = "5vw";
+  });
 }
