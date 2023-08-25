ef19f8ed39f7ccc47f1777bb413cede096485d72
Darcy <darcy.lauder@shaw.ca>
2023-08-25 10:39:07 -0600

    Projects scaffolded

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,16 +1,20 @@
 class LinksController < ApplicationController
-  before_action :check_for_form
+  before_action :check_for_form, :check_for_projects
 
   def show
     @user = User.find(params[:user])
   end
 
   def check_for_form
-    puts "LinksController => params: #{params}"
     if params[:id] == "Contact Form"
       redirect_to new_message_path :position => params[:position]
     end
+  end
 
+  def check_for_projects
+    puts "go to the projects controller"
+    puts "LinkController => params: #{params}"
+    redirect_to projects_path :position => params[:position]
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   has_one_attached :resume
   has_many :navigation_links, class_name: "NavigationLink",
   dependent: :destroy
+  has_many :projects,
+  class_name: "Project",
+  dependent: :destroy
 end
